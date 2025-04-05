b826c5eea0cc6eee16e4c1c63575151daa05fa04
FES001 <firdavs7794948@gmail.com>
2025-04-05 20:48:45 +0500

    Working on it.

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-// QTYDK2XMT4N4JY9A2ECH92J7D
+/* const fetchPromise = fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Uzbekistan?unitGroup=us&key=QTYDK2XMT4N4JY9A2ECH92J7D&contentType=json'); */
+const filterButton = document.querySelector('#filterButton');
+const filterDropDownContainer = document.querySelector('.main-output-header-weather-days-drop-down');
+let isFilterMenuDropped = false;
 
+// FILTER DROPDOWN
 
-const fetchPromise = fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Uzbekistan?unitGroup=us&key=QTYDK2XMT4N4JY9A2ECH92J7D&contentType=json');
+function handlingTheFilterDropDown(e) {
+    e.stopPropagation();
 
+    if (isFilterMenuDropped === false) {
+        filterDropDownContainer.classList.add('main-output-header-weather-days-drop-down-active');
+        
+        isFilterMenuDropped = true;
+    } else {
+        filterDropDownContainer.classList.remove('main-output-header-weather-days-drop-down-active');
+        
+        isFilterMenuDropped = false;
+    };
+};
 
-fetchPromise
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        };
+window.addEventListener('click', () =>{
+    filterDropDownContainer.classList.remove('main-output-header-weather-days-drop-down-active');
+    isFilterMenuDropped = false;
+});
 
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+filterDropDownContainer.addEventListener('click', e => e.stopPropagation());
+
+// INITIALIZING BUTTONS
+filterButton.addEventListener('click', handlingTheFilterDropDown);
