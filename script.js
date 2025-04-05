ceea2941f1f0d59bdf0f87393f68a3322f856608
FES001 <firdavs7794948@gmail.com>
2025-04-05 21:14:16 +0500

    Working on it.

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-/* const fetchPromise = fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Uzbekistan?unitGroup=us&key=QTYDK2XMT4N4JY9A2ECH92J7D&contentType=json'); */
+// FILTER
 const filterButton = document.querySelector('#filterButton');
 const filterDropDownContainer = document.querySelector('.main-output-header-weather-days-drop-down');
 let isFilterMenuDropped = false;
+
+// WEATHER
+const feelsLikeText = document.querySelector('#feelsLikeText');
+const humidityText = document.querySelector('#humidityText');
+const windText = document.querySelector('#windText');
+const snowText = document.querySelector('#snowText');
+const sunriseText = document.querySelector('#sunriseText');
+const sunsetText = document.querySelector('#sunsetText');
+const date = new Date().toISOString().slice(0, 10);
+let currentDate = date;
+
+console.log(currentDate);
 
 // RETRIEVING WEATHER DATA
 
@@ -13,13 +25,29 @@ async function retrievingWeatherData() {
 
         const data = await response.json();
 
-        console.log(data);
+        filterData(data);
     } catch(error) {
         console.log(error);
     };
 };
 
 retrievingWeatherData();
+
+
+// FILTERING THE DATA
+
+function filterData(data) {
+    for (let i = 0; i < data.days.length; i++) {
+        const dropDownInner = document.createElement('div');
+        dropDownInner.classList.add('main-output-header-weather-days-drop-down-inner');
+        dropDownInner.textContent = data.days[i].datetime;
+        if (i === 0) {
+            dropDownInner.classList.add('main-output-header-weather-days-drop-down-inner-active');
+        };
+
+        filterDropDownContainer.appendChild(dropDownInner);
+    };
+};
 
 // FILTER DROPDOWN
 
